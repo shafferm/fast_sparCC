af132ddf120feaa16e8572c9cd30a9ac703e2563
shafferm <michael.shaffer@ucdenver.edu>
2016-07-20 15:13:56 -0600

    Prepare for pypi submission and version .1

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ __author__ = 'shafferm'
 setup(
     name="sparcc_fast",
     version="0.1",
+    author="Michael Shaffer",
+    author_email="michael.shaffer@ucdenver.edu",
+    description="A fast command line interface to find correlations in biom tables with SparCC.",
+    license="BSD",
+    url="https://github.com/shafferm/fast_sparCC",
+
     install_requires=["numpy", "scipy", "biom-format", "pandas"],
     scripts=["scripts/fast_sparCC.py"],
     packages=find_packages()
