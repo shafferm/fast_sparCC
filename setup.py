249fd99d5111eda0c7c345f7847cf5df14f0dfdb
shafferm <michael.shaffer@ucdenver.edu>
2016-07-20 11:07:45 -0600

    Update setup.py to fix changed script name

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     name="sparcc_fast",
     version="0.1",
     install_requires=["numpy", "scipy", "biom-format", "pandas"],
-    scripts=["scripts/sparCC_fast.py"],
+    scripts=["scripts/fast_sparCC.py"],
     packages=find_packages()
 )
