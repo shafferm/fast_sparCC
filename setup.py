8eb61667984cad09086f442ef299c582d0208a8f
shafferm <michael.shaffer@ucdenver.edu>
2017-06-12 10:35:40 -0600

    Update to v0.1.6 to add in function moves

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ __author__ = 'shafferm'
 
 setup(
     name="fast_sparCC",
-    version="v0.1.5",
+    version="v0.1.6",
     author="Michael Shaffer",
     author_email="michael.shaffer@ucdenver.edu",
     description="A fast command line interface to find correlations in biom tables with SparCC.",
     license="BSD",
     url="https://github.com/shafferm/fast_sparCC",
-    download_url="https://github.com/shafferm/fast_sparCC/archive/v0.1.5.tar.gz",
+    download_url="https://github.com/shafferm/fast_sparCC/archive/v0.1.6.tar.gz",
 
     install_requires=["numpy", "scipy", "biom-format", "pandas"],
     scripts=["scripts/fast_sparCC.py"],
