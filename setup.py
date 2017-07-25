9b87e9ab4b7f80ef8e1ab1e4394759e71da79a53
shafferm <michael.shaffer@ucdenver.edu>
2017-07-25 11:45:40 -0600

    Up version to v0.2.0 and fix run_sparcc.py to new function names

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ __author__ = 'shafferm'
 
 setup(
     name="fast_sparCC",
-    version="v0.1.6",
+    version="v0.2.0",
     author="Michael Shaffer",
     author_email="michael.shaffer@ucdenver.edu",
     description="A fast command line interface to find correlations in biom tables with SparCC.",
     license="BSD",
     url="https://github.com/shafferm/fast_sparCC",
-    download_url="https://github.com/shafferm/fast_sparCC/archive/v0.1.6.tar.gz",
+    download_url="https://github.com/shafferm/fast_sparCC/archive/v0.2.0.tar.gz",
 
     install_requires=["numpy", "scipy", "biom-format", "pandas"],
     scripts=["scripts/fast_sparCC.py"],
